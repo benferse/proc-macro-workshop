bab77656105e906e87f57642bc2c6b8f2fa607e3
Ben Fersenheim <ben@bu.edu>
2021-07-16 16:23:16 -0400

    Checkpointing work through builder test 4

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,63 @@
 use proc_macro::TokenStream;
+use quote::{format_ident, quote};
+use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let input = parse_macro_input!(input as DeriveInput);
+    let ident = input.ident;
+    let builder_name = format_ident!("{}Builder", ident);
 
-    unimplemented!()
+    let tokens = quote! {
+        pub struct #builder_name {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #builder_name {
+            pub fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+
+            pub fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+
+            pub fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+
+            pub fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+
+            pub fn build(self) -> Result<#ident, Box<dyn std::error::Error>> {
+                Ok(#ident {
+                    executable: self.executable.ok_or("executable")?,
+                    args: self.args.ok_or("args")?,
+                    env: self.env.ok_or("env")?,
+                    current_dir: self.current_dir.ok_or("current_dir")?,
+                })
+            }
+        }
+
+        impl #ident {
+            pub fn builder() -> #builder_name {
+                #builder_name {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+
+    tokens.into()
 }
